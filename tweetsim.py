fda6044e41ee63e62304447e70cd9fb1467548dd
Tom Bowden <tomfbowden12@gmail.com>
2018-10-11 00:16:02 +1100

    added DM message on api error

--- tweetsim.py
+++ tweetsim.py
@@ -8,6 +8,7 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 bot_name = "@sim_tweet"
+creator_name = "@bo_wd_en"
 debug = False
 
 if "--debug" in sys.argv:
@@ -16,4 +17,9 @@ if "--debug" in sys.argv:
 
 listener = src.listener.StreamListener(bot_name, api, send_tweets=(not debug), verbose=debug)
 stream = tweepy.Stream(auth=api.auth, listener=listener)
-stream.filter(track=[bot_name], async=False)
+
+try:
+    stream.filter(track=[bot_name], async=False)
+except Exception as e:
+    api.send_direct_message(creator_name, "{} broke with the following exception: {}".format(bot_name, e))
+
